cfa48c9708bbfd3c76ad7f7a5988b1d01410c899
DiogoDuque <diogoduque13@hotmail.com>
2017-02-27 12:40:25 +0000

    minor adjustments to score and extra information in questions

--- website/views/question.php
+++ website/views/question.php
@@ -9,9 +9,9 @@
         <div class="post row">
             <!--Score-->
             <div class="score col-md-1">
-                <p class="plus1"><a href="#">+1</a></p>
-                <p>3</p>
-                <p class="minus1"><a href="#">-1</a></p>
+                <span class="glyphicon glyphicon-thumbs-up"></span>
+                <p class="score">3</p>
+                <span class="glyphicon glyphicon-thumbs-down"></span>
             </div>
             <!--Question-->
             <div class="col-md-8">
@@ -20,48 +20,11 @@
                     <p>Aliquam ac turpis vel ligula sodales volutpat at in augue. Maecenas eleifend eros id consectetur fermentum. Ut hendrerit, nulla ut dictum pellentesque, urna enim efficitur urna, sit amet facilisis risus diam nec tellus. Vestibulum egestas justo neque, ac convallis lacus gravida eu. Nunc volutpat, libero at porttitor venenatis, sapien dui sollicitudin urna, ut feugiat sem metus id tortor. Donec vel rutrum nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed odio mauris. Suspendisse porta purus quis libero hendrerit, vel tincidunt diam rutrum. Nam sodales erat sed tellus pellentesque, ac tincidunt eros euismod. Praesent tristique dolor sapien, et interdum orci rhoncus vel. Sed odio sem, vulputate nec vulputate maximus, pharetra non est. Vivamus porta sagittis nunc.</p>
             </div>
             <!--Extra Info-->
-            <div class="col-md-2 offset-md-1"> <!--OFFSET NOT WORKING-->
+            <div class="col-md-2 col-md-offset-1">
                 <p>some xtra stuff, maybe some tags or "resources"</p>
             </div>
         </div>
 
-        <div class="post row">
-            <!--Score-->
-            <div class="score col-md-1">
-                <p class="plus1"><a href="#">+1</a></p>
-                <p>3</p>
-                <p class="minus1"><a href="#">-1</a></p>
-            </div>
-            <!--Question-->
-            <div class="col-md-8">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra feugiat erat posuere pellentesque. Nullam gravida lorem dolor, quis dignissim orci elementum sed. Etiam sollicitudin nunc eu risus tristique tristique. Mauris at massa elit. Fusce ligula tortor, blandit non rhoncus ac, rhoncus vel dui. Suspendisse sit amet sem id sapien volutpat interdum non non orci. Quisque vitae nisl ut nunc fringilla vestibulum. Nam porta porttitor rhoncus. Duis ultrices sem condimentum, suscipit sem vel, faucibus lectus. Nulla pellentesque felis sapien, et porta turpis rhoncus non. Proin nec leo venenatis, iaculis mi id, bibendum lorem. Aenean convallis nec elit vitae scelerisque. Vivamus condimentum nunc ac leo pretium, eu porttitor risus laoreet. Aliquam accumsan ipsum nec eros euismod, non hendrerit felis finibus. Sed scelerisque rhoncus consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-
-                <p>Aliquam ac turpis vel ligula sodales volutpat at in augue. Maecenas eleifend eros id consectetur fermentum. Ut hendrerit, nulla ut dictum pellentesque, urna enim efficitur urna, sit amet facilisis risus diam nec tellus. Vestibulum egestas justo neque, ac convallis lacus gravida eu. Nunc volutpat, libero at porttitor venenatis, sapien dui sollicitudin urna, ut feugiat sem metus id tortor. Donec vel rutrum nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed odio mauris. Suspendisse porta purus quis libero hendrerit, vel tincidunt diam rutrum. Nam sodales erat sed tellus pellentesque, ac tincidunt eros euismod. Praesent tristique dolor sapien, et interdum orci rhoncus vel. Sed odio sem, vulputate nec vulputate maximus, pharetra non est. Vivamus porta sagittis nunc.</p>
-            </div>
-            <!--Extra Info-->
-            <div class="col-md-2 offset-md-1"> <!--OFFSET NOT WORKING-->
-                <p>some xtra stuff, maybe some tags or "resources"</p>
-            </div>
-        </div>
-
-        <div class="post row">
-            <!--Score-->
-            <div class="score col-md-1">
-                <p class="plus1"><a href="#">+1</a></p>
-                <p>3</p>
-                <p class="minus1"><a href="#">-1</a></p>
-            </div>
-            <!--Question-->
-            <div class="col-md-8">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra feugiat erat posuere pellentesque. Nullam gravida lorem dolor, quis dignissim orci elementum sed. Etiam sollicitudin nunc eu risus tristique tristique. Mauris at massa elit. Fusce ligula tortor, blandit non rhoncus ac, rhoncus vel dui. Suspendisse sit amet sem id sapien volutpat interdum non non orci. Quisque vitae nisl ut nunc fringilla vestibulum. Nam porta porttitor rhoncus. Duis ultrices sem condimentum, suscipit sem vel, faucibus lectus. Nulla pellentesque felis sapien, et porta turpis rhoncus non. Proin nec leo venenatis, iaculis mi id, bibendum lorem. Aenean convallis nec elit vitae scelerisque. Vivamus condimentum nunc ac leo pretium, eu porttitor risus laoreet. Aliquam accumsan ipsum nec eros euismod, non hendrerit felis finibus. Sed scelerisque rhoncus consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-
-                <p>Aliquam ac turpis vel ligula sodales volutpat at in augue. Maecenas eleifend eros id consectetur fermentum. Ut hendrerit, nulla ut dictum pellentesque, urna enim efficitur urna, sit amet facilisis risus diam nec tellus. Vestibulum egestas justo neque, ac convallis lacus gravida eu. Nunc volutpat, libero at porttitor venenatis, sapien dui sollicitudin urna, ut feugiat sem metus id tortor. Donec vel rutrum nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed odio mauris. Suspendisse porta purus quis libero hendrerit, vel tincidunt diam rutrum. Nam sodales erat sed tellus pellentesque, ac tincidunt eros euismod. Praesent tristique dolor sapien, et interdum orci rhoncus vel. Sed odio sem, vulputate nec vulputate maximus, pharetra non est. Vivamus porta sagittis nunc.</p>
-            </div>
-            <!--Extra Info-->
-            <div class="col-md-2 offset-md-1"> <!--OFFSET NOT WORKING-->
-                <p>some xtra stuff, maybe some tags or "resources"</p>
-            </div>
-        </div>
     </div>
 
 
