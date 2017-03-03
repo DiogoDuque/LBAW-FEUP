9e98393ba2bdd1c6a01d98fc7c5a09034e8f9c6b
DiogoDuque <diogoduque13@hotmail.com>
2017-03-03 17:09:13 +0000

    question page completed!
    less blank spaces, user and rating are now on the same col, expanded text

--- website/views/question.php
+++ website/views/question.php
@@ -12,21 +12,23 @@
         <!--Title-->
         <h2>Lorem ipsum?</h2>
 
+        <!--Question-->
         <div class="question row">
-			
-            <!--Score-->
-            <div class="col-md-1">
-                <ul class="score-col">
+
+            <div class="col-md-2">
+                <!--User-->
+                <div class="user">
+                    <img alt="User Pic" src="../resources/img/user.png" class="img-circle img-responsive" width="100" height="100">
+                    <a href="#">Peralta</a>
+                </div>
+                <!--Score-->
+                <ul class="score">
                     <li><a class="glyphicon glyphicon-thumbs-up" href="#"></a></li>
-                    <li><p class="score">3</p></li>
+                    <li><p>3</p></li>
                     <li><a class="glyphicon glyphicon-thumbs-down" href="#"></a></li>
                 </ul>
             </div>
-            <!--User-->
-            <div class="user col-md-1">
-                <img alt="User Pic" src="../resources/img/user.png" class="img-circle img-responsive" width="100" height="100">
-                <a href="#">Peralta</a>
-            </div>
+
             <!--Text-->
             <div class="col-md-10">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra feugiat erat posuere pellentesque. Nullam gravida lorem dolor, quis dignissim orci elementum sed. Etiam sollicitudin nunc eu risus tristique tristique. Mauris at massa elit. Fusce ligula tortor, blandit non rhoncus ac, rhoncus vel dui. Suspendisse sit amet sem id sapien volutpat interdum non non orci. Quisque vitae nisl ut nunc fringilla vestibulum. Nam porta porttitor rhoncus. Duis ultrices sem condimentum, suscipit sem vel, faucibus lectus. Nulla pellentesque felis sapien, et porta turpis rhoncus non. Proin nec leo venenatis, iaculis mi id, bibendum lorem. Aenean convallis nec elit vitae scelerisque. Vivamus condimentum nunc ac leo pretium, eu porttitor risus laoreet. Aliquam accumsan ipsum nec eros euismod, non hendrerit felis finibus. Sed scelerisque rhoncus consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
@@ -38,6 +40,7 @@
 					<li><a class="glyphicon glyphicon-trash" href="#" data-toggle="tooltip" title="Remove"></a></li>
 				</ul>
             </div>
+
             <!--Comments-->
             <?php
                 for($j=0;$j<2;$j++) {
@@ -61,21 +64,21 @@
         for($i=0;$i<3;$i++){
         ?>
             <div class="answer row">
-                <!--Score-->
-                <div class="col-md-1">
-                    <ul class="score-col">
+                <div class="col-md-2">
+                    <!--User-->
+                    <div class="user">
+                        <img alt="User Pic" src="../resources/img/user.png" class="img-circle img-responsive" width="100" height="100">
+                        <a href="#">Peralta</a>
+                    </div>
+                    <!--Score-->
+                    <ul class="score">
                         <li><a class="glyphicon glyphicon-thumbs-up" href="#"></a></li>
-                        <li><p class="score">3</p></li>
+                        <li><p>3</p></li>
                         <li><a class="glyphicon glyphicon-thumbs-down" href="#"></a></li>
                     </ul>
                 </div>
-                <!--User-->
-                <div class="user col-md-1">
-                    <img alt="User Pic" src="../resources/img/user.png" class="img-circle img-responsive" width="100" height="100">
-                    <a href="#">Peralta</a>
-                </div>
                 <!--Text-->
-                <div class="col-md-8">
+                <div class="col-md-10">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra feugiat erat posuere pellentesque. Nullam gravida lorem dolor, quis dignissim orci elementum sed. Etiam sollicitudin nunc eu risus tristique tristique. Mauris at massa elit. Fusce ligula tortor, blandit non rhoncus ac, rhoncus vel dui. Suspendisse sit amet sem id sapien volutpat interdum non non orci. Quisque vitae nisl ut nunc fringilla vestibulum. Nam porta porttitor rhoncus. Duis ultrices sem condimentum, suscipit sem vel, faucibus lectus. Nulla pellentesque felis sapien, et porta turpis rhoncus non. Proin nec leo venenatis, iaculis mi id, bibendum lorem. Aenean convallis nec elit vitae scelerisque. Vivamus condimentum nunc ac leo pretium, eu porttitor risus laoreet. Aliquam accumsan ipsum nec eros euismod, non hendrerit felis finibus. Sed scelerisque rhoncus consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
                     Aliquam ac turpis vel ligula sodales volutpat at in augue. Maecenas eleifend eros id consectetur fermentum. Ut hendrerit, nulla ut dictum pellentesque, urna enim efficitur urna, sit amet facilisis risus diam nec tellus. Vestibulum egestas justo neque, ac convallis lacus gravida eu. Nunc volutpat, libero at porttitor venenatis, sapien dui sollicitudin urna, ut feugiat sem metus id tortor. Donec vel rutrum nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed odio mauris. Suspendisse porta purus quis libero hendrerit, vel tincidunt diam rutrum. Nam sodales erat sed tellus pellentesque, ac tincidunt eros euismod. Praesent tristique dolor sapien, et interdum orci rhoncus vel. Sed odio sem, vulputate nec vulputate maximus, pharetra non est. Vivamus porta sagittis nunc.</p>
 					<ul class="actions pull-right">	
@@ -84,15 +87,11 @@
 						<li><a class="glyphicon glyphicon-trash" href="#" data-toggle="tooltip" title="Remove"></a></li>
 					</ul>
                 </div>
-                <!--Extra Info-->
-                <div class="col-md-2">
-                    <p>some xtra stuff, maybe some tags or "resources"</p>
-                </div>
                 <!--Comments-->
                 <?php
                 for($j=0;$j<2;$j++) {
                 ?>
-                    <div class="comment col-md-7 col-md-offset-3">
+                    <div class="comment col-md-9 col-md-offset-3">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra feugiat erat posuere pellentesque. Nullam gravida lorem dolor, quis dignissim orci elementum sed</p>
                             <a href="#">Peralta</a>
 							<ul class="actions pull-right">	
