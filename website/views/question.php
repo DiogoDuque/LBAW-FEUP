3569dd26a551f6fbb6f6d58e035d7e32f56a9460
DiogoDuque <diogoduque13@hotmail.com>
2017-03-03 11:02:02 +0000

    question page now with actions; has 2 different designs (to be decided later)

--- website/views/question.php
+++ website/views/question.php
@@ -1,6 +1,11 @@
 <?php include_once "../templates/logged_in_header.html";?>
 
     <link rel="stylesheet" href="../lib/css/question.css">
+	<script>
+		$(document).ready(function(){
+			$('[data-toggle="tooltip"]').tooltip(); 
+		});
+	</script>
 
     <!--Content-->
     <div class="container">
@@ -16,10 +21,6 @@
                     <li><p class="score">3</p></li>
                     <li><a class="glyphicon glyphicon-thumbs-down" href="#"></a></li>
                 </ul>
-				<ul class="actions">	
-					<li><a class="glyphicon glyphicon-link" href="#"><br>Share</a></li>
-					<li><a class="glyphicon glyphicon-flag" href="#"><br>Report</a></li>
-				</ul>
             </div>
             <!--User-->
             <div class="user col-md-1">
@@ -27,21 +28,29 @@
                 <a href="#">Peralta</a>
             </div>
             <!--Text-->
-            <div class="col-md-8">
+            <div class="col-md-10">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra feugiat erat posuere pellentesque. Nullam gravida lorem dolor, quis dignissim orci elementum sed. Etiam sollicitudin nunc eu risus tristique tristique. Mauris at massa elit. Fusce ligula tortor, blandit non rhoncus ac, rhoncus vel dui. Suspendisse sit amet sem id sapien volutpat interdum non non orci. Quisque vitae nisl ut nunc fringilla vestibulum. Nam porta porttitor rhoncus. Duis ultrices sem condimentum, suscipit sem vel, faucibus lectus. Nulla pellentesque felis sapien, et porta turpis rhoncus non. Proin nec leo venenatis, iaculis mi id, bibendum lorem. Aenean convallis nec elit vitae scelerisque. Vivamus condimentum nunc ac leo pretium, eu porttitor risus laoreet. Aliquam accumsan ipsum nec eros euismod, non hendrerit felis finibus. Sed scelerisque rhoncus consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
                     Aliquam ac turpis vel ligula sodales volutpat at in augue. Maecenas eleifend eros id consectetur fermentum. Ut hendrerit, nulla ut dictum pellentesque, urna enim efficitur urna, sit amet facilisis risus diam nec tellus. Vestibulum egestas justo neque, ac convallis lacus gravida eu. Nunc volutpat, libero at porttitor venenatis, sapien dui sollicitudin urna, ut feugiat sem metus id tortor. Donec vel rutrum nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed odio mauris. Suspendisse porta purus quis libero hendrerit, vel tincidunt diam rutrum. Nam sodales erat sed tellus pellentesque, ac tincidunt eros euismod. Praesent tristique dolor sapien, et interdum orci rhoncus vel. Sed odio sem, vulputate nec vulputate maximus, pharetra non est. Vivamus porta sagittis nunc.</p>
-            </div>
-            <!--Extra Info-->
-            <div class="col-md-2">
-                <p>some xtra stuff, maybe some tags or "resources"</p>
+				<ul class="actions pull-right">	
+					<li><a class="glyphicon glyphicon-link" href="#" data-toggle="tooltip" title="Share"></a></li>
+					<li><a class="glyphicon glyphicon-flag" href="#" data-toggle="tooltip" title="Report"></a></li>
+					<li><a class="glyphicon glyphicon-pencil" href="#" data-toggle="tooltip" title="Edit"></a></li>
+					<li><a class="glyphicon glyphicon-trash" href="#" data-toggle="tooltip" title="Remove"></a></li>
+				</ul>
             </div>
             <!--Comments-->
             <?php
                 for($j=0;$j<2;$j++) {
             ?>
-                    <div class="comment col-md-6 col-md-offset-4">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra feugiat erat posuere pellentesque. Nullam gravida lorem dolor, quis dignissim orci elementum sed<br>
-                            <a href="#">Peralta</a></p>
+                    <div class="comment col-md-9 col-md-offset-3">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra feugiat erat posuere pellentesque. Nullam gravida lorem dolor, quis dignissim orci elementum sed</p>
+                            <a href="#">Peralta</a>
+							<ul class="actions pull-right">	
+								<li><a class="glyphicon glyphicon-flag" href="#" data-toggle="tooltip" title="Report"></a></li>
+								<li><a class="glyphicon glyphicon-pencil" href="#" data-toggle="tooltip" title="Edit"></a></li>
+								<li><a class="glyphicon glyphicon-trash" href="#" data-toggle="tooltip" title="Remove"></a></li>
+							</ul>
+						
                     </div>
             <?php
                 }
@@ -53,13 +62,12 @@
         ?>
             <div class="answer row">
                 <!--Score-->
-                <div class="col-md-1 col-md-offset-1">
+                <div class="col-md-1">
                     <ul class="score-col">
                         <li><a class="glyphicon glyphicon-thumbs-up" href="#"></a></li>
                         <li><p class="score">3</p></li>
                         <li><a class="glyphicon glyphicon-thumbs-down" href="#"></a></li>
                     </ul>
-					<a class="glyphicon glyphicon-flag" href="#"><br>Report</a>
                 </div>
                 <!--User-->
                 <div class="user col-md-1">
@@ -67,9 +75,14 @@
                     <a href="#">Peralta</a>
                 </div>
                 <!--Text-->
-                <div class="col-md-7">
+                <div class="col-md-8">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra feugiat erat posuere pellentesque. Nullam gravida lorem dolor, quis dignissim orci elementum sed. Etiam sollicitudin nunc eu risus tristique tristique. Mauris at massa elit. Fusce ligula tortor, blandit non rhoncus ac, rhoncus vel dui. Suspendisse sit amet sem id sapien volutpat interdum non non orci. Quisque vitae nisl ut nunc fringilla vestibulum. Nam porta porttitor rhoncus. Duis ultrices sem condimentum, suscipit sem vel, faucibus lectus. Nulla pellentesque felis sapien, et porta turpis rhoncus non. Proin nec leo venenatis, iaculis mi id, bibendum lorem. Aenean convallis nec elit vitae scelerisque. Vivamus condimentum nunc ac leo pretium, eu porttitor risus laoreet. Aliquam accumsan ipsum nec eros euismod, non hendrerit felis finibus. Sed scelerisque rhoncus consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
                     Aliquam ac turpis vel ligula sodales volutpat at in augue. Maecenas eleifend eros id consectetur fermentum. Ut hendrerit, nulla ut dictum pellentesque, urna enim efficitur urna, sit amet facilisis risus diam nec tellus. Vestibulum egestas justo neque, ac convallis lacus gravida eu. Nunc volutpat, libero at porttitor venenatis, sapien dui sollicitudin urna, ut feugiat sem metus id tortor. Donec vel rutrum nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed odio mauris. Suspendisse porta purus quis libero hendrerit, vel tincidunt diam rutrum. Nam sodales erat sed tellus pellentesque, ac tincidunt eros euismod. Praesent tristique dolor sapien, et interdum orci rhoncus vel. Sed odio sem, vulputate nec vulputate maximus, pharetra non est. Vivamus porta sagittis nunc.</p>
+					<ul class="actions pull-right">	
+						<li><a class="glyphicon glyphicon-flag" href="#" data-toggle="tooltip" title="Report"></a></li>
+						<li><a class="glyphicon glyphicon-pencil" href="#" data-toggle="tooltip" title="Edit"></a></li>
+						<li><a class="glyphicon glyphicon-trash" href="#" data-toggle="tooltip" title="Remove"></a></li>
+					</ul>
                 </div>
                 <!--Extra Info-->
                 <div class="col-md-2">
@@ -79,9 +92,14 @@
                 <?php
                 for($j=0;$j<2;$j++) {
                 ?>
-                    <div class="comment col-md-6 col-md-offset-4">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra feugiat erat posuere pellentesque. Nullam gravida lorem dolor, quis dignissim orci elementum sed<br>
-                            <a href="#">Peralta</a></p>
+                    <div class="comment col-md-7 col-md-offset-3">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra feugiat erat posuere pellentesque. Nullam gravida lorem dolor, quis dignissim orci elementum sed</p>
+                            <a href="#">Peralta</a>
+							<ul class="actions pull-right">	
+								<li><a class="glyphicon glyphicon-flag" href="#" data-toggle="tooltip" title="Report"></a></li>
+								<li><a class="glyphicon glyphicon-pencil" href="#" data-toggle="tooltip" title="Edit"></a></li>
+								<li><a class="glyphicon glyphicon-trash" href="#" data-toggle="tooltip" title="Remove"></a></li>
+							</ul>
                     </div>
                 <?php
                 }
