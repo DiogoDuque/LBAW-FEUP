35d9b26b2dbad72d4b946d5153b3ddbc009ed262
José Aleixo <josealeixo.pc@gmail.com>
2017-03-04 17:46:46 +0000

    Made a cycle in the home page that iterates through questions, instead of repeating code.

--- website/views/home.php
+++ website/views/home.php
@@ -1,4 +1,22 @@
-<?php include_once "../templates/header.php"; ?>
+<?php include_once "../templates/header.php";
+
+$questions = array(
+    "Why are the only possible values for a bit 0 and 1?",
+    "Best side dish for a filet mignon?",
+    "Do you like this?",
+    "What are the best Billiards training practices?"
+);
+
+$questions_categories = array(
+    "Computers",
+    "Food and Drinks",
+    "Food and Drinks",
+    "Sports"
+);
+
+$questions_points = array(4, -2, 5, 0);
+
+?>
 
     <!--Content-->
     <div class="container">
@@ -9,67 +27,35 @@
             <li><a data-toggle="tab" href="#controverse">Most Controversial</a></li>
         </ul>
 
+
         <!--Tabs Content-->
         <div class="tab-content">
+
+            <!--MOST RECENT -->
             <div id="recent" class="tab-pane fade in active">
+
+                <?php
+
+                for ($i = 0;
+                     $i < count($questions);
+                     $i++) {
+                ?>
+
                 <!--Multi Link-->
                 <div class="container answer">
                     <div class="row">
-                        <div class="col-sm-9 pre">
-                            <h4><a href="../views/question.php" class="home-question-title">Why are the only possible
-                                    values for a bit 0 and 1?</a><br>
-                                <small>asked 55 seconds ago by <a href="profile.php">Peralta</a> in <a>Computers</a>
+                        <div class="col-sm-10 pre">
+                            <h4><a href="../views/question.php"
+                                   class="home-question-title"><?= $questions[$i] ?></a><br>
+                                <small>asked 55 seconds ago by <a href="profile.php">Peralta</a> in
+                                    <a><?= $questions_categories[$i] ?></a>
                                 </small>
-                            </h4>
-                        </div>
-                        <div class="col-sm-1">
-                            <h4 class="text-center">
-                                <small>
-                                    <span class="glyphicon glyphicon-thumbs-up"></span>
-                                    2
-                                    <span class="glyphicon glyphicon-thumbs-up"></span>
-                                    upvotes
-                                </small>
-                            </h4>
-                        </div>
-                        <div class="col-sm-1">
-                            <h4 class="text-center">
-                                <small>
-                                    <span class="glyphicon glyphicon-thumbs-down"></span>
-                                    5
-                                    <span class="glyphicon glyphicon-thumbs-down"></span>
-                                    downvotes
-                                </small>
-                            </h4>
-                        </div>
-                        <div class="col-sm-1">
-                            <h4 class="text-center">
-                                <small>
-                                    <span class="glyphicon glyphicon-comment"></span>
-                                    0
-                                    <span class="glyphicon glyphicon-comment"></span>
-                                    answers
-                                </small>
-                            </h4>
-                        </div>
-                    </div>
-                </div>
-
-                <hr class="main-menu-questions-divider">
-
-                <!--Single Link-->
-                <div class="container answer">
-                    <div class="row">
-                        <div class="question col-sm-10 pre">
-                            <h4><a class="home-question-title">Best side dish for a filet mignon?</a><br>
-                                <small>asked 55 seconds ago by <a href="profile.php">Peralta</a> in <a>Food and
-                                        Drinks</a></small>
                             </h4>
                         </div>
 
                         <div class="points col-sm-1">
                             <h4 class="text-center">
-                                <div class="points">-3</div>
+                                <div class="points"><?= $questions_points[$i] ?></div>
                                 <small>points</small>
                             </h4>
                         </div>
@@ -87,94 +73,22 @@
                                 </small>
                             </h4>
                         </div>
+
                     </div>
                 </div>
 
-                <hr class="main-menu-questions-divider">
-
-                <!--Single Link-->
-                <div class="container answer">
-                    <div class="row">
-                        <div class="question col-sm-10 pre">
-                            <h4><a class="home-question-title">Do you like this?</a><br>
-                                <small>asked 55 seconds ago by <a href="profile.php">Peralta</a> in <a>Food and
-                                        Drinks</a></small>
-                            </h4>
-                        </div>
-
-                        <div class="points col-sm-1">
-                            <h4 class="text-center">
-                                <div class="points">5</div>
-                                <small>points</small>
-                            </h4>
-                        </div>
-
-
-                        <div class="answers col-sm-1">
-                            <h4 class="text-center">
-                                <div class="answers">
-                                    0
-                                    <small><span class="glyphicon glyphicon-comment"></span></small>
-                                </div>
-
-                                <small>
-                                    answers
-                                </small>
-                            </h4>
-                        </div>
-                    </div>
-                </div>
-
-                <hr class="main-menu-questions-divider">
-
-                <!--Single Link-->
-                <div class="container answer">
-                    <div class="row">
-                        <div class="col-sm-9 pre">
-                            <h4><a class="home-question-title">What are the best Billiards training practices?</a><br>
-                                <small>asked 55 seconds ago by <a href="profile.php">Peralta</a> in <a>Sports</a>
-                                </small>
-                            </h4>
-                        </div>
-                        <div class="col-sm-1">
-                            <h4 class="text-center">
-                                <small>
-                                    <span class="glyphicon glyphicon-thumbs-up"></span>
-                                    2
-                                    <span class="glyphicon glyphicon-thumbs-up"></span>
-                                    upvotes
-                                </small>
-                            </h4>
-                        </div>
-                        <div class="col-sm-1">
-                            <h4 class="text-center">
-                                <small>
-                                    <span class="glyphicon glyphicon-thumbs-down"></span>
-                                    5
-                                    <span class="glyphicon glyphicon-thumbs-down"></span>
-                                    downvotes
-                                </small>
-                            </h4>
-                        </div>
-                        <div class="col-sm-1">
-                            <h4 class="text-center">
-                                <small>
-                                    <span class="glyphicon glyphicon-comment"></span>
-                                    0
-                                    <span class="glyphicon glyphicon-comment"></span>
-                                    answers
-                                </small>
-                            </h4>
-                        </div>
-                    </div>
-                </div>
+                <?php } ?>
 
             </div>
 
+            <hr class="main-menu-questions-divider">
+
+            <!--MOST POPULAR-->
             <div id="popular" class="tab-pane fade">
                 <p>Some content.</p>
             </div>
 
+            <!--MOST CONTROVERSE-->
             <div id="controverse" class="tab-pane fade">
                 <!--Single Link-->
                 <div class="container answer">
