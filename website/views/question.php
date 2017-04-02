287b457267af6df011f28fe5d6aa172ac3d1ef43
José Aleixo <josealeixo.pc@gmail.com>
2017-04-02 17:42:30 +0100

    Added edit.php for post edition and some more images.

--- website/views/question.php
+++ website/views/question.php
@@ -96,8 +96,8 @@
                 <div class="widget-area no-padding blank">
                     <div class="comment-box">
                         <form>
-                            <textarea placeholder="Comment" ></textarea>
-                            <button type="submit" class="btn btn-success green">Comment</button>
+                            <textarea placeholder="Comment" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra feugiat erat posuere pellentesque. Nullam gravida lorem dolor, quis dignissim orci elementum sed</textarea>
+                            <button type="submit" class="btn btn-success green">Apply</button>
                         </form>
                     </div><!-- Status Upload  -->
                 </div><!-- Widget Area -->
