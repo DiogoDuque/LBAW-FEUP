2aea95df4d19862cbf70e56bd1d9de4636de3b50
José Aleixo <josealeixo.pc@gmail.com>
2017-04-14 22:03:33 +0100

    Sign up gives exception

--- website/actions/auth/signup.php
+++ website/actions/auth/signup.php
@@ -8,5 +8,11 @@
     $email = $_POST["email"];
     $password = $_POST["password"];
 
-    createUser($username, $password, $email);
+    if(createUser($username, $password, $email) != 0){
+        header( "refresh:2;url={$BASE_URL}" );
+    }
+    else{
+        header("Location: {$BASE_URL}");
+        exit();
+    }
 
--- website/database/members.php
+++ website/database/members.php
@@ -1,15 +1,48 @@
 <?php
 
+function checkIfUsernameExists($username){
+    global $conn;
+    try {
+        $stmt = $conn->prepare("SELECT * FROM public.member WHERE username = ?");
+        $stmt->execute(array($username));
+
+        $list_of_names = $stmt->fetchAll();
+
+        if(count($list_of_names) != 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    catch (PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+}
+
 function createUser($username, $password, $email) {
     global $conn;
-    $stmt = $conn->prepare("INSERT INTO member (username, email, hashed_pass)VALUES (?, ?, ?)");
+
+
+    if(checkIfUsernameExists($username))
+    {
+        echo "Username already exists.";
+        return -1;
+    }
+
     try {
+        $stmt = $conn->prepare("INSERT INTO member (username, email, hashed_pass)VALUES (?, ?, ?)");
         $stmt->execute(array($username, $email, sha1($password)));
     }
     catch (PDOException $e)
     {
-
+        echo $e->getMessage();
     }
+
+    return 0;
 }
 
 function isLoginCorrect($username, $password) {
