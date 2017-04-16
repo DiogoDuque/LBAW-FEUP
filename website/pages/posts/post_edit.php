433c7641913c7ed87ffce06299556c17d94d4273
José Aleixo <josealeixo.pc@gmail.com>
2017-04-16 18:12:41 +0100

    Edited post needs to be tested.

--- website/pages/posts/post_edit.php
+++ website/pages/posts/post_edit.php
@@ -1,36 +1,27 @@
 <?php
 
 include_once("../../config/init.php");
+include_once ($BASE_DIR."database/DatabaseGetter.php");
 
 $smarty->display("common/header.tpl");
+
+$getter = new DatabaseGetter();
+
+$post = $getter->getPost($_GET["id"]);
+$currentVersion = $getter->getLatestPostVersion($_GET["id"]);
 
 ?>
 
 <div class="container">
     <h1 class="text-center">Edit Post</h1>
 
+    <!--TODO: Put text to a PHP POST.-->
     <form>
+        <input type="hidden" name="editedText" id="editedText" value="">
         <div class="new-answer">
-            <div id="summernote" class="formgroup"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra feugiat erat posuere
-                pellentesque. Nullam gravida lorem dolor, quis dignissim orci elementum sed. Etiam
-                sollicitudin nunc eu risus tristique tristique. Mauris at massa elit. Fusce ligula tortor,
-                blandit non rhoncus ac, rhoncus vel dui. Suspendisse sit amet sem id sapien volutpat
-                interdum non non orci. Quisque vitae nisl ut nunc fringilla vestibulum. Nam porta porttitor
-                rhoncus. Duis ultrices sem condimentum, suscipit sem vel, faucibus lectus. Nulla
-                pellentesque felis sapien, et porta turpis rhoncus non. Proin nec leo venenatis, iaculis mi
-                id, bibendum lorem. Aenean convallis nec elit vitae scelerisque. Vivamus condimentum nunc ac
-                leo pretium, eu porttitor risus laoreet. Aliquam accumsan ipsum nec eros euismod, non
-                hendrerit felis finibus. Sed scelerisque rhoncus consectetur. Orci varius natoque penatibus
-                et magnis dis parturient montes, nascetur ridiculus mus.
-                Aliquam ac turpis vel ligula sodales volutpat at in augue. Maecenas eleifend eros id
-                consectetur fermentum. Ut hendrerit, nulla ut dictum pellentesque, urna enim efficitur urna,
-                sit amet facilisis risus diam nec tellus. Vestibulum egestas justo neque, ac convallis lacus
-                gravida eu. Nunc volutpat, libero at porttitor venenatis, sapien dui sollicitudin urna, ut
-                feugiat sem metus id tortor. Donec vel rutrum nibh. Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Suspendisse sed odio mauris. Suspendisse porta purus quis libero hendrerit,
-                vel tincidunt diam rutrum. Nam sodales erat sed tellus pellentesque, ac tincidunt eros
-                euismod. Praesent tristique dolor sapien, et interdum orci rhoncus vel. Sed odio sem,
-                    vulputate nec vulputate maximus, pharetra non est. Vivamus porta sagittis nunc.</p></div>
+            <div id="summernote" class="formgroup">
+                    <?=$currentVersion["text"]?>
+            </div>
             <script>
                 $(document).ready(function() {
                     $('#summernote').summernote();
@@ -38,9 +29,21 @@ $smarty->display("common/header.tpl");
             </script>
         </div>
 
-        <button type="submit" class="btn btn-success">Apply</button>
-        <button type="reset" class="btn btn-danger">Reset</button>
+        <button type="submit" onclick="sendEditedText()" class="btn btn-success">Apply</button>
+        <button type="reset" onclick="resetEditedText()" class="btn btn-danger">Reset</button>
     </form>
 </div>
+
+<script>
+    function sendEditedText(){
+        document.editedText.value = $('#summernote').code();
+        document.forms["sampleForm"].submit();
+    }
+
+    function resetEditedText(){
+        document.editedText.value = <?= json_encode($currentVersion["text"], JSON_HEX_TAG);?>;
+        document.forms["sampleForm"].submit();
+    }
+</script>
 
 <?php $smarty->display("common/footer.tpl"); ?>
