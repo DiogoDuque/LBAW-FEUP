fd8707fa1730bfadfc7d58a6731b6a5f6b44845c
José Aleixo <josealeixo.pc@gmail.com>
2017-04-24 09:06:15 +0100

    Update config

--- proto/config/init.php
+++ proto/config/init.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ERROR); // E_NOTICE by default
 
 $BASE_DIR = "/opt/lbaw/lbaw1623/public_html/LBAW-FEUP/proto/"; // == proto/
