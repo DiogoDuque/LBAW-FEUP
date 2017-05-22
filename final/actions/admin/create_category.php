b0831e74c9a279ab1e35827e0a117a2724d9eccf
filpez <joaofilipepereiracosta@hotmail.com>
2017-05-22 12:43:19 +0100

    Permissions to create/remove categories

--- final/actions/admin/create_category.php
+++ final/actions/admin/create_category.php
@@ -3,6 +3,11 @@
 include_once ("../../config/init.php");
 include_once ($BASE_DIR."database/categories.php");
 
+//Check permissions
+$member = getMemberByUsername($_SESSION['username']);
+if($member['privilege_level'] != "Administrator")
+    die('You don\'t have permissions for perform this action...');
+
 $name = $_POST['name'];
 $id = addCategory($name);
 
--- final/actions/admin/remove_category.php
+++ final/actions/admin/remove_category.php
@@ -3,6 +3,11 @@
 include_once ("../../config/init.php");
 include_once ($BASE_DIR."database/categories.php");
 
+//Check permissions
+$member = getMemberByUsername($_SESSION['username']);
+if($member['privilege_level'] != "Administrator")
+    die('You don\'t have permissions for perform this action...');
+
 $id = $_POST['id'];
 $id = deleteCategory($id);
 
